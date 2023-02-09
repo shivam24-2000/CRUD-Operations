d5ace6084af4ca9e5694adcd49699d5a0f404007
shivam24-2000 <shivamsinghal24@gmail.com>
2023-02-09 22:47:51 +0530

    CRUD

---
+++ CRUDOperation/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUDOperation.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Category { get; set; }
+
+        public int isActive { get; set;}
+    }
+}
---
+++ CRUDOperation/Program.cs
@@ -0,0 +1,31 @@
+using CRUDOperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Adding SQL Conncetions 
+builder.Services.AddDbContext<BrandContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BrandCS"))
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
